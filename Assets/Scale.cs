f0214b1f271d97b501179bb70099b962cbe9a14f
MercyKuba <akuwork0801@gmail.com>
2021-03-04 22:52:56 -0600

    Added rest

---
+++ Assets/Scale.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scale : MonoBehaviour
+{
+    public GameObject form;
+    public Slider scale;
+    public float previousValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.scale.onValueChanged.AddListener(this.OnSliderChanged);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnSliderChanged(float value)
+    {
+        
+        
+            form.transform.localScale = new Vector3(value, value, value);
+
+       
+    }
+}
---
+++ Assets/Switching.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switching : MonoBehaviour
+{
+    public GameObject a;
+    public GameObject b;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ToggleModel();
+    }
+
+    // Update is called once per frame
+    void Update(){       
+        
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            ToggleModel();
+        }
+
+     
+
+    }
+    void ToggleModel()
+
+    {
+        Debug.Log("Test");
+        a.SetActive(!a.activeSelf);
+        b.SetActive(!b.activeSelf);
+
+    }
+}
